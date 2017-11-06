e472e2d95cb53b2f6729d131a9f203727dcf0504
Zeeshannu <mzeeshanmzee@gmail.com>
2017-11-06 23:30:16 +0500

    Toast is added

--- app/src/main/java/com/example/zeeshan/a4november2017_saturday_lab/MainActivity.java
+++ app/src/main/java/com/example/zeeshan/a4november2017_saturday_lab/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void add_records(View view) {
+        Toast.makeText(this, "THis is upfsted", Toast.LENGTH_SHORT).show();
         Intent intent=new Intent(getApplicationContext(),AddActivity.class);
         startActivity(intent);
 
