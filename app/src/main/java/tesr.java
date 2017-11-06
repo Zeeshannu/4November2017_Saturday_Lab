2071beaaa7a470166e7d9dcf54eecff1bf54e73c
Zeeshannu <mzeeshanmzee@gmail.com>
2017-11-07 00:13:10 +0500

    tesr f is added

--- app/src/main/java/tesr.java
+++ app/src/main/java/tesr.java
@@ -3,4 +3,18 @@
  */
 
 public class tesr {
+    public int f;
+
+    public tesr(int f) {
+
+        this.f = f;
+    }
+
+    public int getF() {
+        return f;
+    }
+
+    public void setF(int f) {
+        this.f = f;
+    }
 }
